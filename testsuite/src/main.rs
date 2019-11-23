daebffe80cee760c39ec2a785985cc3c82f63736
Pierre-Antoine Champin <pchampin@liris.cnrs.fr>
2019-11-23 16:13:04 +0100

    added gtrig to the test-suite
    For this, I improved the ergonomics of testsuite/src/main.rs:
    the second parameter (manifest URL) can now be the name of a format,
    in which case the correct manifest URL is automatically set.
    
    This allows to use the same manifest URL for different formats
    (in this case, TriG et GTriG).

--- testsuite/src/main.rs
+++ testsuite/src/main.rs
@@ -1,7 +1,9 @@
 use rio_testsuite::manifest::TestManifest;
+use rio_testsuite::model::OwnedDataset;
 use rio_testsuite::parser_evaluator::*;
 use rio_testsuite::report::TestOutcome;
 use std::env;
+use std::error::Error;
 use std::path::Path;
 
 fn main() {
@@ -10,12 +12,38 @@ fn main() {
         eprintln!("Expecting two arguments: <w3c_rdf_tests_base_path> <manifest_url>");
         return;
     }
+    let mut parse_func: fn(&str, &Path) -> Result<OwnedDataset, Box<dyn Error>> = parse_w3c_rdf_test_file;
+    let mut manifest_url: &str = &args[2];
+
+    if args[2] == "ntriples" || args[2] == "nt" {
+        manifest_url =  "http://w3c.github.io/rdf-tests/ntriples/manifest.ttl";
+    }
+    if args[2] == "nquads" || args[2] == "nq" {
+        manifest_url =  "http://w3c.github.io/rdf-tests/nquads/manifest.ttl";
+    }
+    if args[2] == "turtle" || args[2] == "ttl" {
+        manifest_url =  "http://w3c.github.io/rdf-tests/turtle/manifest.ttl";
+    }
+    if args[2] == "rdf-xml" || args[2] == "rdf" {
+        manifest_url =  "http://w3c.github.io/rdf-tests/rdf-xml/manifest.ttl";
+    }
+    if args[2] == "trig" {
+        manifest_url =  "http://w3c.github.io/rdf-tests/trig/manifest.ttl";
+    }
+    #[cfg(feature = "generalized")]
+    {
+        if args[2] == "gtrig" {
+            parse_func = parse_w3c_rdf_test_file_with_gtrig;
+            manifest_url =  "http://w3c.github.io/rdf-tests/trig/manifest.ttl";
+        }
+    }
+
     let test_path = Path::new(&args[1]);
-    let manifest = TestManifest::new(args[2].clone(), |url| {
-        parse_w3c_rdf_test_file(url, &test_path)
+    let manifest = TestManifest::new(manifest_url.to_string(), |url| {
+        parse_func(url, &test_path)
     });
 
-    match evaluate_parser_tests(manifest, |url| parse_w3c_rdf_test_file(url, &test_path)) {
+    match evaluate_parser_tests(manifest, |url| parse_func(url, &test_path)) {
         Ok(results) => {
             for result in results {
                 match result.outcome {
