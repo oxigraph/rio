98a08a97ae98b327811f4d72e42b341ce8450047
Thomas Tanon <Tpt@users.noreply.github.com>
2022-01-11 19:54:36 +0100

    Improves MAX_STACK_SIZE documentation
    Closes #76

--- turtle/src/lib.rs
+++ turtle/src/lib.rs
@@ -72,4 +72,7 @@ pub use gtrig::GTriGParser;
 mod sophia;
 
 /// Maximal number of nested structures (collections, blank node, quoted triples...).
+///
+/// This limit is set in order to avoid stack overflow error when parsing such structures due to too many recursive calls.
+/// The actual limit value is a wet finger compromise between not failing to parse valid files and avoiding to trigger stack overflow errors.
 const MAX_STACK_SIZE: usize = 128;
